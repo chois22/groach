effa37d5bdc1004a122236c950ffb6a000058370
gaemi91_apple <lepoete91@gmail.com>
2025-04-22 10:11:51 +0900

    리뷰 작성 텍스트 필드 테스트

--- lib/ui/route/review/route_review_write.dart
+++ lib/ui/route/review/route_review_write.dart
@@ -44,212 +44,225 @@ class _RouteReviewWriteState extends State<RouteReviewWrite> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('리뷰 쓰기', style: TS.s18w600(colorBlack))),
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Gaps.v16,
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                child: Row(
-                  children: [
-                    Container(
-                      width: 60,
-                      height: 60,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(12),
+    return GestureDetector(
+      onTap: (){
+        FocusManager.instance.primaryFocus?.unfocus();
+      },
+      child: Scaffold(
+        appBar: AppBar(title: Text('리뷰 쓰기', style: TS.s18w600(colorBlack))),
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Gaps.v16,
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: Row(
+                    children: [
+                      Container(
+                        width: 60,
+                        height: 60,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        clipBehavior: Clip.hardEdge,
+                        child: Image.asset(
+                          widget.modelProgram.listImgUrl.first,
+                          fit: BoxFit.cover,
+                        ),
                       ),
-                      clipBehavior: Clip.hardEdge,
-                      child: Image.asset(
-                        widget.modelProgram.listImgUrl.first,
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                    Gaps.h8,
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(widget.modelProgram.name, style: TS.s15w500(colorGray600)),
-                          Gaps.v5,
-                          Text(
-                            widget.modelProgram.desc,
-                            style: TS.s16w400(
-                              colorBlack,
+                      Gaps.h8,
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(widget.modelProgram.name, style: TS.s15w500(colorGray600)),
+                            Gaps.v5,
+                            Text(
+                              widget.modelProgram.desc,
+                              style: TS.s16w400(
+                                colorBlack,
+                              ),
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: 2,
                             ),
-                            overflow: TextOverflow.ellipsis,
-                            maxLines: 2,
-                          ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Gaps.v26,
+                CustomDivider(color: colorGray200, height: 10),
+                Gaps.v26,
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: Column(
+                    children: [
+                      Text('별점을 눌러 만족도를 알려주세요.', style: TS.s18w600(colorGray900)),
+                      Gaps.v16,
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: List.generate(5, (index) {
+                          return ValueListenableBuilder(
+                            valueListenable: vnSelectedStar,
+                            builder: (context, selectedStar, child) {
+                              String assetName = index < selectedStar
+                                  ? 'assets/icon/yellow_star_icon.svg'
+                                  : 'assets/icon/grey_star_icon.svg';
+                              return GestureDetector(
+                                onTap: () {
+                                  vnSelectedStar.value = index + 1;
+                                },
+                                child: Padding(
+                                  padding: const EdgeInsets.only(right: 2),
+                                  child: SvgPicture.asset(
+                                    assetName,
+                                    width: 28,
+                                    height: 28,
+                                  ),
+                                ),
+                              );
+                            },
+                          );
+                        }),
+                      ),
+                      ValueListenableBuilder(
+                        valueListenable: vnSelectedStar,
+                        builder: (context, selectedStar, child) {
+                          return Padding(
+                            padding: const EdgeInsets.only(top: 8.0, bottom: 20),
+                            child: Text('$selectedStar.0', style: TS.s24w700(Color(0xFFFAAD14))),
+                          );
+                        },
+                      ),
+                      CustomDivider(color: colorGray200, height: 1),
+                      Gaps.v20,
+
+                      /// 리뷰 체크 부분
+                      Column(
+                        children: List.generate(5, (index) {
+                          return ValueListenableBuilder(
+                            valueListenable: vnListSelectedReviewBoxes[index],
+                            builder: (context, listSelectedReviewBoxes, child) {
+                              return GestureDetector(
+                                onTap: () {
+                                  vnListSelectedReviewBoxes[index].value = !listSelectedReviewBoxes;
+                                },
+                                child: Column(
+                                  children: [
+                                    ReviewBox(
+                                      iconPath: UtilsEnum.getNameFromReviewKeyWordIcon(ReviewKeyWord.values[index]),
+                                      text: UtilsEnum.getNameFromReviewKeyWord(ReviewKeyWord.values[index]),
+                                      textStyle:
+                                          listSelectedReviewBoxes ? TS.s14w700(colorGreen600) : TS.s14w500(colorBlack),
+                                      boxColor: listSelectedReviewBoxes ? colorGreen50 : colorWhite,
+                                      boxBorderColor: listSelectedReviewBoxes ? colorGreen600 : colorGray200,
+                                      boxBorderWidth: 1.0,
+                                    ),
+                                    Gaps.v8,
+                                  ],
+                                ),
+                              );
+                            },
+                          );
+                        }),
+                      ),
+                      Gaps.v12,
+                      Row(
+                        children: [
+                          Text('리뷰 작성', style: TS.s14w500(colorGray700)),
                         ],
                       ),
-                    ),
-                  ],
-                ),
-              ),
-              Gaps.v26,
-              CustomDivider(color: colorGray200, height: 10),
-              Gaps.v26,
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                child: Column(
-                  children: [
-                    Text('별점을 눌러 만족도를 알려주세요.', style: TS.s18w600(colorGray900)),
-                    Gaps.v16,
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: List.generate(5, (index) {
-                        return ValueListenableBuilder(
-                          valueListenable: vnSelectedStar,
-                          builder: (context, selectedStar, child) {
-                            String assetName = index < selectedStar ? 'assets/icon/yellow_star_icon.svg' : 'assets/icon/grey_star_icon.svg';
-                            return GestureDetector(
-                              onTap: () {
-                                vnSelectedStar.value = index + 1;
-                              },
-                              child: Padding(
-                                padding: const EdgeInsets.only(right: 2),
-                                child: SvgPicture.asset(
-                                  assetName,
-                                  width: 28,
-                                  height: 28,
-                                ),
-                              ),
-                            );
-                          },
-                        );
-                      }),
-                    ),
-                    ValueListenableBuilder(
-                      valueListenable: vnSelectedStar,
-                      builder: (context, selectedStar, child) {
-                        return Padding(
-                          padding: const EdgeInsets.only(top: 8.0, bottom: 20),
-                          child: Text('$selectedStar.0', style: TS.s24w700(Color(0xFFFAAD14))),
-                        );
-                      },
-                    ),
-                    CustomDivider(color: colorGray200, height: 1),
-                    Gaps.v20,
-
-                    /// 리뷰 체크 부분
-                    Column(
-                      children: List.generate(5, (index) {
-                        return ValueListenableBuilder(
-                          valueListenable: vnListSelectedReviewBoxes[index],
-                          builder: (context, listSelectedReviewBoxes, child) {
-                            return GestureDetector(
-                              onTap: () {
-                                vnListSelectedReviewBoxes[index].value = !listSelectedReviewBoxes;
-                              },
-                              child: Column(
-                                children: [
-                                  ReviewBox(
-                                    iconPath: UtilsEnum.getNameFromReviewKeyWordIcon(ReviewKeyWord.values[index]),
-                                    text: UtilsEnum.getNameFromReviewKeyWord(ReviewKeyWord.values[index]),
-                                    textStyle: listSelectedReviewBoxes ? TS.s14w700(colorGreen600) : TS.s14w500(colorBlack),
-                                    boxColor: listSelectedReviewBoxes ? colorGreen50 : colorWhite,
-                                    boxBorderColor: listSelectedReviewBoxes ? colorGreen600 : colorGray200,
-                                    boxBorderWidth: 1.0,
-                                  ),
-                                  Gaps.v8,
-                                ],
-                              ),
-                            );
-                          },
-                        );
-                      }),
-                    ),
-                    Gaps.v12,
-                    Row(
-                      children: [
-                        Text('리뷰 작성', style: TS.s14w500(colorGray700)),
-                      ],
-                    ),
-                    Gaps.v8,
-                    ValueListenableBuilder(
-                      valueListenable: tecReviewWrite,
-                      builder: (context, _, child) => TextFieldDefault(
-                        controller: tecReviewWrite,
-                        focusNode: focusNode,
-                        hintText: '리뷰를 작성해 주세요.',
-                        colorBorder: focusNode.hasFocus ? colorGreen600 : colorGray200,
-                        minLines: 8,
-                        maxLines: null,
+                      Gaps.v8,
+                      ValueListenableBuilder(
+                        valueListenable: tecReviewWrite,
+                        builder: (context, _, child) => SizedBox(
+                          height: 250,
+                          child: TextFieldDefault(
+                            controller: tecReviewWrite,
+                            focusNode: focusNode,
+                            hintText: '리뷰를 작성해 주세요.',
+                            colorBorder: focusNode.hasFocus ? colorGreen600 : colorGray200,
+                            textAlignVertical: TextAlignVertical.top,
+                            maxLines: null,
+                            expands: true,
+                            maxLength: 20,
+                          ),
+                        ),
                       ),
-                    ),
-                    Gaps.v20,
-                    Row(
-                      children: [
-                        Text('사진 업로드', style: TS.s14w500(colorGray700)),
-                        Text('(최대 5장)', style: TS.s14w500(colorGray500)),
-                      ],
-                    ),
-                    Gaps.v8,
-                    SizedBox(
-                      child: SingleChildScrollView(
-                        scrollDirection: Axis.horizontal,
-                        child: Row(
-                          children: List.generate(
-                            5,
-                            (index) => Row(
-                              children: [
-                                Container(
-                                  width: 100,
-                                  height: 100,
-                                  decoration: BoxDecoration(
-                                    color: colorWhite,
-                                    borderRadius: BorderRadius.circular(12),
-                                    border: Border.all(color: colorGray300, width: 1),
-                                  ),
-                                  child: GestureDetector(
-                                    onTap: () {
-                                      print('사진 업로드');
-                                    },
-                                    child: Column(
-                                      mainAxisAlignment: MainAxisAlignment.center,
-                                      children: [
-                                        SvgPicture.asset('assets/icon/circle+.svg'),
-                                        Gaps.v8,
-                                        Text('사진 업로드', style: TS.s13w500(colorGray500)),
-                                      ],
+                      Gaps.v20,
+                      Row(
+                        children: [
+                          Text('사진 업로드', style: TS.s14w500(colorGray700)),
+                          Text('(최대 5장)', style: TS.s14w500(colorGray500)),
+                        ],
+                      ),
+                      Gaps.v8,
+                      SizedBox(
+                        child: SingleChildScrollView(
+                          scrollDirection: Axis.horizontal,
+                          child: Row(
+                            children: List.generate(
+                              5,
+                              (index) => Row(
+                                children: [
+                                  Container(
+                                    width: 100,
+                                    height: 100,
+                                    decoration: BoxDecoration(
+                                      color: colorWhite,
+                                      borderRadius: BorderRadius.circular(12),
+                                      border: Border.all(color: colorGray300, width: 1),
+                                    ),
+                                    child: GestureDetector(
+                                      onTap: () {
+                                        print('사진 업로드');
+                                      },
+                                      child: Column(
+                                        mainAxisAlignment: MainAxisAlignment.center,
+                                        children: [
+                                          SvgPicture.asset('assets/icon/circle+.svg'),
+                                          Gaps.v8,
+                                          Text('사진 업로드', style: TS.s13w500(colorGray500)),
+                                        ],
+                                      ),
                                     ),
                                   ),
-                                ),
-                                Builder(
-                                  builder: (context) {
-                                    if (index == 4) {
-                                      return const SizedBox.shrink();
-                                    } else {
-                                      return Gaps.h10;
-                                    }
-                                  },
-                                )
-                              ],
+                                  Builder(
+                                    builder: (context) {
+                                      if (index == 4) {
+                                        return const SizedBox.shrink();
+                                      } else {
+                                        return Gaps.h10;
+                                      }
+                                    },
+                                  )
+                                ],
+                              ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                    Gaps.v16,
-                    GestureDetector(
-                      onTap: (){
-                        FocusManager.instance.primaryFocus?.unfocus();
-                        showDialog(
-                          context: context,
-                          builder: (context) => DialogCancelConfirm(
-                            text: '리뷰를 등록하시겠어요?',
-                          ),
-                        );
-                      },
-                      child: ButtonAnimate(title: '작성 완료', colorBg: colorGreen600),
-                    ),
-                    Gaps.v16,
-                  ],
+                      Gaps.v16,
+                      GestureDetector(
+                        onTap: () {
+                          FocusManager.instance.primaryFocus?.unfocus();
+                          showDialog(
+                            context: context,
+                            builder: (context) => DialogCancelConfirm(
+                              text: '리뷰를 등록하시겠어요?',
+                            ),
+                          );
+                        },
+                        child: ButtonAnimate(title: '작성 완료', colorBg: colorGreen600),
+                      ),
+                      Gaps.v16,
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
